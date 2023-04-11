34b810217e52d3495a870b93e812b3375bfeaab8
Jobe Paul Authement <jobeauthement@gmail.com>
2023-04-10 19:13:35 -0500

    added ability to search for movies and on results

--- resources/views/pages/results.blade.php
+++ resources/views/pages/results.blade.php
@@ -14,8 +14,9 @@
             </div>
             <div
                 class="p-0.5 mb-7 border max-w-lg mx-auto bg-gradient-cyan focus-within:ring focus-within:ring-indigo-400 overflow-hidden rounded-xl">
-                <div class="p-0.5 flex flex-col md:flex-row bg-white overflow-hidden rounded-lg">
-                    <input
+                <form method="GET" action="/search"
+                    class="p-0.5 flex flex-col md:flex-row bg-white overflow-hidden rounded-lg">
+                    <input name="query"
                         class="block flex-1 px-5 py-4 md:py-0 text-base text-gray-500 bg-transparent outline-none placeholder-gray-500 rounded-tl-xl rounded-bl-xl"
                         type="text" placeholder="Enter a Movie Or TV Show" contenteditable="false"><button
                         class="group relative font-heading font-semibold w-full md:w-auto py-5 px-8 text-xs text-white bg-gray-900 hover:bg-gray-800 uppercase overflow-hidden rounded-md tracking-px">
@@ -24,7 +25,7 @@
                         </div>
                         <p class="relative z-10">Search</p>
                     </button>
-                </div>
+                </form>
             </div>
 
         </div>
@@ -36,55 +37,26 @@
                 class="mb-6 max-w-max mx-auto text-center text-transparent bg-clip-text bg-gradient-cyan2 font-heading text-xs uppercase font-semibold tracking-px">
                 HERE'S YOUR RESULTS
             </p>
-            <h2 class="mb-20 font-heading font-semibold text-center text-6xl sm:text-7xl text-white">Star Wars
+            <h2 class="mb-20 font-heading font-semibold text-center text-6xl sm:text-7xl text-white">{{ $query }}
             </h2>
             <div class="flex flex-wrap -m-3">
-                <div class="w-full md:w-1/2 xl:w-1/4 p-3">
-                    <div class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
-                        <img class="mx-auto w-full" src="gradia-assets/images/teams/avatar-xl.png" alt="">
-                        <div class="absolute bottom-0 left-0 w-full p-2.5">
-                            <div class="p-5 w-full bg-white rounded-md">
-                                <h2 class="font-heading font-bold text-lg text-gray-900">Star Wars
-                                </h2>
-                                <p class="text-sm text-gray-600">Sci-fi
-                                </p>
+                @foreach ($results as $item)
+                    <div class="poster w-full md:w-1/2 xl:w-1/4 p-3">
+                        <a href="/{{ $item['tmdb_type'] }}/{{ $item['id'] }}"
+                            class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
+                            <img class="mx-auto w-full" src="{{ $item['image_url'] }}" alt="">
+                            <div class="absolute bottom-0 left-0 w-full p-2.5">
+                                <div class="p-5 w-full bg-white rounded-md">
+                                    <h2 class="font-heading font-bold text-lg text-gray-900">{{ $item['name'] }}
+                                    </h2>
+                                    <p class="text-sm text-gray-600">{{ $item['tmdb_type'] }}
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/4 p-3">
-                    <div class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
-                        <img class="mx-auto w-full" src="gradia-assets/images/teams/avatar-xl2.png" alt="">
-                        <div class="absolute bottom-0 left-0 w-full p-2.5">
-                            <div class="p-5 w-full bg-white rounded-md">
-                                <h2 class="font-heading font-bold text-lg text-gray-900">Eleanor Pena</h2>
-                                <p class="text-sm text-gray-600">Co-Founder, CTO</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/4 p-3">
-                    <div class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
-                        <img class="mx-auto w-full" src="gradia-assets/images/teams/avatar-xl3.png" alt="">
-                        <div class="absolute bottom-0 left-0 w-full p-2.5">
-                            <div class="p-5 w-full bg-white rounded-md">
-                                <h2 class="font-heading font-bold text-lg text-gray-900">Devon Lane</h2>
-                                <p class="text-sm text-gray-600">Chief Marketing Officer</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/4 p-3">
-                    <div class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
-                        <img class="mx-auto w-full" src="gradia-assets/images/teams/avatar-xl4.png" alt="">
-                        <div class="absolute bottom-0 left-0 w-full p-2.5">
-                            <div class="p-5 w-full bg-white rounded-md">
-                                <h2 class="font-heading font-bold text-lg text-gray-900">Robert Fox</h2>
-                                <p class="text-sm text-gray-600">Senior Software Developer</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+
             </div>
         </div>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -42,12 +42,17 @@ Route::get('/', function () {
 // Define a route that handles GET requests to the /search URL path. When a search request is received, the closure passed as the second argument to Route::get builds a URL for the Watchmode API based on the user's search query input. The URL includes the API key for the Watchmode API, the search field (which is set to "name" in this case), and the search value (which is set to the user's search query input). This URL is returned as a string. In addition to the URL, the closure could also return a view that displays the search results to the user.
 Route::get('/search', function (Request $request) {
     // return $request->input('query');
-    $query = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=" . env('WATCHMODE_KEY') . "&search_field=name&search_value=" . $request->input('query');
+    $query = $request->input('query');
+    $request_url = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=" . env('WATCHMODE_KEY') . "&search_field=name&search_value=" . $query;
 
 
-    $response = Http::get($query);
-    return $response->json();
-    return view('pages.results');
+    $response = Http::get($request_url);
+    $results = $response->json();
+    $results = $results['results'];
+    return view('pages.results', [
+        "results" => $results,
+        "query" => ucwords($query)
+    ]);
 });
 
 Route::get('/movie', function () {
